119694cbd9301fb14e735fd5d0fec14f9d504b49
Muhammad Adnan <dononlineboss@gmail.com>
2024-03-24 17:10:45 +0500

    modified code

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-const randomNumber = Math.floor(Math.random() * 6 + 1);
-const answer = await inquirer.prompt([
-    {
-        name: "userGuessedNumber",
-        type: "number",
-        message: "Please guess a number between 1-6:"
-    }
-]);
-if (answer.userGuessedNumber > 6 || answer.userGuessedNumber < 1) {
-    console.log("Number should be between 1 to 6");
-}
-else {
-    if (answer.userGuessedNumber === randomNumber) {
-        console.log("Congratulations! You guessed right number.");
+let playAgain = true;
+while (playAgain) {
+    const randomNumber = Math.floor(Math.random() * 6 + 1);
+    const answer = await inquirer.prompt([
+        {
+            name: "userGuessedNumber",
+            type: "number",
+            message: "Please guess a number between 1-6:"
+        }
+    ]);
+    if (answer.userGuessedNumber > 6 || answer.userGuessedNumber < 1) {
+        console.log("Number should be between 1 to 6");
     }
     else {
-        console.log("Sorry! You guessed wrong number.");
-        console.log(`Random number is: ${randomNumber} and Your guessed number is: ${answer.userGuessedNumber}`);
+        if (answer.userGuessedNumber === randomNumber) {
+            console.log("Congratulations! You guessed right number.");
+        }
+        else {
+            console.log("Sorry! You guessed wrong number.");
+            console.log(`Random number is: ${randomNumber} and Your guessed number is: ${answer.userGuessedNumber}`);
+        }
     }
+    const playAgainAnswer = await inquirer.prompt([
+        {
+            name: "playAgain",
+            type: "confirm",
+            message: "Do you wish to play again?"
+        }
+    ]);
+    playAgain = playAgainAnswer.playAgain;
 }
--- index.ts
+++ index.ts
@@ -2,25 +2,38 @@
 
 import inquirer from "inquirer";
 
+let playAgain = true;
 
-const randomNumber: number = Math.floor(Math.random() * 6 + 1);
+while (playAgain) {
 
-const answer = await inquirer.prompt([
-    {
-        name: "userGuessedNumber",
-        type: "number",
-        message: "Please guess a number between 1-6:"
-    }
-])
+    const randomNumber: number = Math.floor(Math.random() * 6 + 1);
 
-if (answer.userGuessedNumber > 6 || answer.userGuessedNumber < 1) {
-    console.log("Number should be between 1 to 6")
-} else {
-    if (answer.userGuessedNumber === randomNumber) {
-        console.log("Congratulations! You guessed right number.")
+    const answer = await inquirer.prompt([
+        {
+            name: "userGuessedNumber",
+            type: "number",
+            message: "Please guess a number between 1-6:"
+        }
+    ])
+
+    if (answer.userGuessedNumber > 6 || answer.userGuessedNumber < 1) {
+        console.log("Number should be between 1 to 6")
+    } else {
+        if (answer.userGuessedNumber === randomNumber) {
+            console.log("Congratulations! You guessed right number.")
+        }
+        else {
+            console.log("Sorry! You guessed wrong number.")
+            console.log(`Random number is: ${randomNumber} and Your guessed number is: ${answer.userGuessedNumber}`)
+        }
     }
-    else {
-        console.log("Sorry! You guessed wrong number.")
-        console.log(`Random number is: ${randomNumber} and Your guessed number is: ${answer.userGuessedNumber}`)
-    }
+
+    const playAgainAnswer = await inquirer.prompt([
+        {
+            name: "playAgain",
+            type: "confirm",
+            message: "Do you wish to play again?"
+        }
+    ])
+    playAgain = playAgainAnswer.playAgain;
 }
